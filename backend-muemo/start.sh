8136880fe670178769045e4812ddce4cbbdde6bf
unknown <samuelyudigun@gmail.com>
2025-05-05 02:04:52 +0700

    [SY] Add some scripts

---
+++ backend-muemo/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Ensure venv exists
+if [ ! -d "/opt/venv" ]; then
+  python -m venv /opt/venv
+  . /opt/venv/bin/activate
+  pip install -r requirements.txt
+fi
+# Run your app
+python app.py
